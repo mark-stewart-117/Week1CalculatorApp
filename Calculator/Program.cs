10b7b10e61310ce2f52f44447494e5a617ebd186
mark-stewart-117 <stewart-m22@ulster.ac.uk>
2025-09-28 17:18:44 +0100

    Implemented program code.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Variable names should be short and meaningful
+
+//Ask for input for first number
+Console.WriteLine("Type in the first number followed by the Enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+//Ask for input for second number
+Console.WriteLine("Type in the second number followed by the Enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//Perform calculation
+int result = firstNumber + secondNumber;
+
+//Output answer
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
