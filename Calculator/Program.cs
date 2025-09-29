b49067544b8d9f9df9a7a9e1dab5d90ff75bbc0c
mark-stewart-117 <stewart-m22@ulster.ac.uk>
2025-09-29 09:05:12 +0100

    Added else ifs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,11 +12,11 @@ void CalculatorApp()
 
     //Ask for input for first number
     Console.WriteLine("Type in the first number followed by the Enter key");
-     firstNumber = Convert.ToInt32(Console.ReadLine());
+    firstNumber = Convert.ToInt32(Console.ReadLine());
 
     //Ask for input for second number
     Console.WriteLine("Type in the second number followed by the Enter key");
-     secondNumber = Convert.ToInt32(Console.ReadLine());
+    secondNumber = Convert.ToInt32(Console.ReadLine());
 
     //Use a loop to make the relevant decision
     //and perform the maths operation
@@ -29,9 +29,31 @@ void CalculatorApp()
     //Convert string to integer
     choice = Convert.ToInt32(Console.ReadLine());
 
-    //Perform calculation
-     result = firstNumber + secondNumber;
-
-    //Output answer
-    Console.WriteLine("The result is {result}", result);
+    //Use IF statement to perform calculation
+    if (choice == 1)
+    {
+        result = firstNumber + secondNumber;
+        Console.WriteLine($"Adding {firstNumber} and {secondNumber} equals {result}");
+    }
+    else if (choice == 2)
+    {
+        result = firstNumber - secondNumber;
+        Console.WriteLine($"Subtracting {firstNumber} from {secondNumber} equals {result}");
+    }
+    else if (choice == 3)
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"Dividing {firstNumber} by {secondNumber} equals {result}");
+    }
+    else if (choice == 4)
+    {
+        result = firstNumber * secondNumber;
+        Console.WriteLine($"Multiplying {firstNumber} and {secondNumber} equals {result}");
+    }
+    else
+    {
+        Console.WriteLine("You did not select a valid number between 1-4");
+    }
+        //Output answer
+        Console.WriteLine("The result is {result}", result);
 }
